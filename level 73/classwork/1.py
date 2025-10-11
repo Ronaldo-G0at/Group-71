6d0042ce664df8583acabc35fcf925fe4434ea8f
Ronaldo-G0at <labadzeshio8@gmail.com>
2025-10-11 21:11:43 +0400

    L

---
+++ level 73/classwork/1.py
@@ -0,0 +1,4 @@
+# def get_age(age):
+#     return int(age[0])
+# def summation(num):
+#     return sum(range(1, num +1))
---
+++ level 73/homework/cd.py
@@ -0,0 +1,27 @@
+# def get_average(marks):
+#     marks1 = sum(marks)
+#     marks2= len(marks)
+#     res=marks1 / marks2
+#     res1=int(res)
+#     return res1
+# def rps(p1, p2):
+#     if p1 == p2:
+#         return "Draw!"
+#     elif p1 == "rock" and p2 == "paper":
+#         return "Player 2 won!"
+#     elif p1 == "scissors" and p2 == "paper":
+#         return "Player 1 won!"
+#     elif p1 == "paper" and p2 == "scissors":
+#         return "Player 2 won!"
+#     elif p1 == "rock" and p2 == "scissors":
+#         return "Player 1 won!"
+#     elif p1 == "scissors" and p2 == "rock":
+#         return "Player 2 won!"
+#     elif p1 == "paper" and p2 == "rock":
+#         return "Player 1 won!"
+# def plural(n):
+#     return n!=1
+# def bonus_time(salary, bonus):
+#     return f"${salary * 10 if bonus else salary}"
+# def say_hello(name):
+#     return f"Hello, {name}"
